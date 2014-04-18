478b22a65a47729b09a8c7edd43bbb224b08da32
Matthew Dean <deanmatthew16@gmail.com>
2014-04-18 10:53:47 -0400

    Lots of quotes on scripts thanks to shellcheck.net

--- scripts/push_db.sh
+++ scripts/push_db.sh
@@ -2,7 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-source $DIR/settings.sh
+source "$DIR"/settings.sh
 
 if [ ! -f "$DIR/$DB_NAME" ]; then
 	echo >&2 "The comics database ($DB_NAME) was not found. Did you pull it yet?"
@@ -10,4 +10,4 @@ if [ ! -f "$DIR/$DB_NAME" ]; then
 fi
 
 
-adb push $DIR/$DB_NAME $ANDROID_LOCATION
+adb push "$DIR/$DB_NAME" "$ANDROID_LOCATION"
